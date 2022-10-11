b8003f52120c97f75b6e5000eec8c692a1fd7e57
hnko <henzikou@gmail.com>
2022-10-11 16:01:10 -0700

    refactor: use Qiime primitive type

--- q2_humann3/plugin_setup.py
+++ q2_humann3/plugin_setup.py
@@ -2,7 +2,7 @@ import qiime2.plugin
 from q2_types.feature_table import FeatureTable, Frequency, RelativeFrequency
 from q2_types.per_sample_sequences import SequencesWithQuality
 from q2_types.sample_data import SampleData
-from qiime2.plugin import SemanticType
+from qiime2.plugin import Choices, Int, SemanticType, Str
 
 import q2_humann3
 from q2_humann3._format import (Bowtie2IndexDirFmt2, HumannDbDirFormat,
@@ -59,16 +59,43 @@ plugin.methods.register_function(
         "bowtie_database": Bowtie2Index2,
     },
     parameters={
-        "threads": qiime2.plugin.Int,
-        "memory_use": qiime2.plugin.Str,
-        "metaphlan_options": qiime2.plugin.Str,
+        "threads": Int,
+        "memory_use": Str % Choices({"minimum", "maximum"}),
+        "metaphlan_options": Str,
     },
-    name="Characterize samples using HUMAnN2",
     outputs=[
         ("genefamilies", FeatureTable[Frequency]),  # type: ignore
         ("pathcoverage", FeatureTable[RelativeFrequency]),  # type: ignore
         ("pathabundance", FeatureTable[RelativeFrequency]),  # type: ignore
         ("taxonomy", FeatureTable[RelativeFrequency]),  # type: ignore
     ],
+    input_descriptions={
+        "demultiplexed_seqs": "--- UPDATE ---",
+        "nucleotide_database": "directory containing the nucleotide database",
+        "protein_database": "directory containing the protein database",
+        "pathway_database": "directory providing a tab-delimited mapping",
+        "pathway_mapping": "directory providing the pathways mapping",
+        "bowtie_database": "directory containing the bowtie2 executable",
+    },
+    parameter_descriptions={
+        "threads": "number of threads/processes",
+        "memory_use": "the amount of memory to use",
+        "metaphlan_options": "options to be provided to the MetaPhlAn software",
+    },
+    output_descriptions={
+        "genefamilies": ("This file details the abundance of each gene family"
+                         " in the community."),
+        "pathcoverage": ("Pathway coverage provides an alternative description"
+                         " of the presence (1) and absence (0) of pathways in"
+                         " a community, independent of their quantitative"
+                         " abundance."),
+        "pathabundance": ("This file details the abundance of each pathway in"
+                          " the community as a function of the abundances of"
+                          " the pathway's component reactions, with each"
+                          " reaction's abundance computed as the sum over"
+                          " abundances of genes catalyzing the reaction."),
+        "taxonomy": ("--- UPDATE ---"),
+    },
+    name="Characterize samples using HUMAnN2",
     description="Execute the HUMAnN2",
 )
